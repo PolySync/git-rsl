96d3ef41991f66a3703bde2d445052579a413e3c
Jeff Weiss <jweiss@polysync.io>
2017-08-08 16:09:31 -0700

    change option parsing to clap
    Prior to this commit we were using getopts for option parsing, which,
    while it made it easy to detect options passed to the application,
    getopts didn't have any functionality for stating that certain arguments
    were required or the cardinality of the those arguments. This meant that
    we'd have to manually traverse the command line arguments and do that
    validation ourselves. This commit replaces the usage of getopt with clap
    in order to delegate that work of argument validation and cardinality.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-extern crate getopts;
+#[macro_use]
+extern crate clap;
+
 extern crate git2;
 
 use std::env;
 
-use getopts::Options;
 use git2::Repository;
 
 mod common;
@@ -15,10 +16,6 @@ fn discover_repo() -> Result<Repository, git2::Error> {
     let current_dir = env::current_dir().unwrap();
     Repository::discover(current_dir)
 }
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} [options]", program);
-    print!("{}", opts.usage(&brief));
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -26,30 +23,24 @@ fn main() {
 
     let repo = discover_repo().unwrap();
 
-    let mut opts = Options::new();
+    let matches = clap_app!(git_rsl =>
+                            (name: program.clone())
+                            (version: "0.1")
+                            (about: "Uses a reference state log to secure fetch and push")
+                            (@group mode =>
+                                (@arg fetch: --fetch "Securely fetch <branch> checking the reference state log")
+                                (@arg push: --push "Securely push <branch> updating the reference state log")
+                             )
+                            (@arg remote: +required "Remote repository (example: origin)")
+                            (@arg branch: ... +required "Branch(es) to securely fetch or push (example: master)")
+                            ).get_matches();
 
-    opts.optflag("h", "help", "print this help menu");
-    opts.optflag("", "fetch", "securely fetch, adding new entry to RSL nonce bag");
-    opts.optflag("", "push", "securely push, logging this push in the RSL");
-
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(f) => panic!(f.to_string()),
-    };
-
-    if matches.opt_present("h") {
-        print_usage(&program, opts);
-        return;
-    }
-
-    if program == "git-securefetch" || matches.opt_present("fetch") {
+    println!("matches: {:?}", matches);
+    if program == "git-securefetch" || matches.is_present("fetch") {
         fetch::secure_fetch(&repo);
         return;
-    } else if program == "git-securepush" || matches.opt_present("push") {
+    } else if program == "git-securepush" || matches.is_present("push") {
         push::secure_push(&repo);
-        return;
-    } else {
-        print_usage(&program, opts);
         return;
     }
 }
