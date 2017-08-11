5111e718056b9eb8bd92afbffffbb72d3b7f88fd
Jeff Weiss <jweiss@polysync.io>
2017-08-11 16:35:50 -0700

    add authentication callbacks
    Prior to this commit fetches would fail because we weren't even
    attempting to authentication. This commit steals the
    `with_authentication` function from cargo to reach into the ssh agent or
    git credential helper and pass the appropriate credentials through to
    libgit2.

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -2,9 +2,11 @@ extern crate crypto;
 extern crate rand;
 
 use std::collections::HashSet;
+use std::process;
 use std::vec::Vec;
 
-use git2::{Oid, Reference, Remote, Repository};
+use git2;
+use git2::{Cred, FetchOptions, Oid, ProxyOptions, Reference, Remote, RemoteCallbacks, Repository};
 use rand::Rng;
 
 mod push_entry;
@@ -15,16 +17,49 @@ pub use self::nonce::HasNonce;
 
 const RSL_BRANCH: &'static str = "RSL";
 const NONCE_BRANCH: &'static str = "RSL_NONCE";
+const REFLOG_MSG: &'static str = "Retrieve RSL branchs from remote";
 
 pub fn retrieve_rsl_and_nonce_bag_from_remote_repo<'repo>(repo: &'repo Repository, remote: &mut Remote) -> (Reference<'repo>, HashSet<Nonce>) {
 
-    remote.fetch(&[RSL_BRANCH, NONCE_BRANCH], None, None);
+    let cfg = repo.config().unwrap();
+    let remote_copy = remote.clone();
+    let url = remote_copy.url().unwrap();
+
+    with_authentication(url, &cfg, |f| {
+        let mut cb = RemoteCallbacks::new();
+        cb.credentials(f);
+        let mut opts = FetchOptions::new();
+        opts.remote_callbacks(cb);
+
+        remote.fetch(&[RSL_BRANCH, NONCE_BRANCH], Some(&mut opts), Some(REFLOG_MSG))
+    });
+
     let remote_name = remote.name().unwrap();
     let remote_rsl_ref_name = format!("{}/{}", remote_name, RSL_BRANCH);
-    let remote_rsl = repo.find_reference(&remote_rsl_ref_name).unwrap();
+    let remote_rsl = match repo.find_reference(&remote_rsl_ref_name) {
+        Ok(r) => r,
+        Err(e) => {
+            println!("Error: could not find remote Reference State Log Push Entry branch '{}'", remote_rsl_ref_name);
+            println!("This remote has not been initialized with a Reference State Log Push Entry branch");
+            println!("Please run git-rsl --push");
+            println!("If you have already done this, then this remote may be compromised.");
+            println!("  {}", e);
+            process::exit(99);
+        }
+    };
 
     let remote_nonce_ref_name = format!("{}/{}", remote_name, NONCE_BRANCH);
-    let remote_nonce = repo.find_reference(&remote_nonce_ref_name).unwrap();
+    let remote_nonce = match repo.find_reference(&remote_nonce_ref_name) {
+        Ok(r) => r,
+        Err(e) => {
+            println!("Error: could not find remote Reference State Log Nonce branch '{}'", remote_nonce_ref_name);
+            println!("This remote has not been initialized with a Reference State Log Nonce branch");
+            println!("Please run git-rsl --push");
+            println!("If you have already done this, then this remote may be compromised.");
+            println!("  {}", e);
+            process::exit(98);
+        }
+    };
 
     let nonce_bag = read_nonce_bag(&remote_nonce);
 
@@ -92,3 +127,153 @@ fn read_nonce_bag(remote_nonce: &Reference) -> HashSet<Nonce> {
 
 }
 
+fn with_authentication<T, F>(url: &str, cfg: &git2::Config, mut f: F)
+                             -> Result<T, ::git2::Error>
+    where F: FnMut(&mut git2::Credentials) -> Result<T, ::git2::Error>
+{
+    let mut cred_helper = git2::CredentialHelper::new(url);
+    cred_helper.config(cfg);
+
+    let mut ssh_username_requested = false;
+    let mut cred_helper_bad = None;
+    let mut ssh_agent_attempts = Vec::new();
+    let mut any_attempts = false;
+    let mut tried_sshkey = false;
+
+    let mut res = f(&mut |url, username, allowed| {
+        any_attempts = true;
+        // libgit2's "USERNAME" authentication actually means that it's just
+        // asking us for a username to keep going. This is currently only really
+        // used for SSH authentication and isn't really an authentication type.
+        // The logic currently looks like:
+        //
+        //      let user = ...;
+        //      if (user.is_null())
+        //          user = callback(USERNAME, null, ...);
+        //
+        //      callback(SSH_KEY, user, ...)
+        //
+        // So if we're being called here then we know that (a) we're using ssh
+        // authentication and (b) no username was specified in the URL that
+        // we're trying to clone. We need to guess an appropriate username here,
+        // but that may involve a few attempts. Unfortunately we can't switch
+        // usernames during one authentication session with libgit2, so to
+        // handle this we bail out of this authentication session after setting
+        // the flag `ssh_username_requested`, and then we handle this below.
+        if allowed.contains(git2::USERNAME) {
+            debug_assert!(username.is_none());
+            ssh_username_requested = true;
+            return Err(git2::Error::from_str("gonna try usernames later"))
+        }
+
+        // An "SSH_KEY" authentication indicates that we need some sort of SSH
+        // authentication. This can currently either come from the ssh-agent
+        // process or from a raw in-memory SSH key. Cargo only supports using
+        // ssh-agent currently.
+        //
+        // If we get called with this then the only way that should be possible
+        // is if a username is specified in the URL itself (e.g. `username` is
+        // Some), hence the unwrap() here. We try custom usernames down below.
+        if allowed.contains(git2::SSH_KEY) && !tried_sshkey {
+            // If ssh-agent authentication fails, libgit2 will keep
+            // calling this callback asking for other authentication
+            // methods to try. Make sure we only try ssh-agent once,
+            // to avoid looping forever.
+            tried_sshkey = true;
+            let username = username.unwrap();
+            debug_assert!(!ssh_username_requested);
+            ssh_agent_attempts.push(username.to_string());
+            return git2::Cred::ssh_key_from_agent(username)
+        }
+
+        // Sometimes libgit2 will ask for a username/password in plaintext. This
+        // is where Cargo would have an interactive prompt if we supported it,
+        // but we currently don't! Right now the only way we support fetching a
+        // plaintext password is through the `credential.helper` support, so
+        // fetch that here.
+        if allowed.contains(git2::USER_PASS_PLAINTEXT) {
+            let r = git2::Cred::credential_helper(cfg, url, username);
+            cred_helper_bad = Some(r.is_err());
+            return r
+        }
+
+        // I'm... not sure what the DEFAULT kind of authentication is, but seems
+        // easy to support?
+        if allowed.contains(git2::DEFAULT) {
+            return git2::Cred::default()
+        }
+
+        // Whelp, we tried our best
+        Err(git2::Error::from_str("no authentication available"))
+    });
+
+    // Ok, so if it looks like we're going to be doing ssh authentication, we
+    // want to try a few different usernames as one wasn't specified in the URL
+    // for us to use. In order, we'll try:
+    //
+    // * A credential helper's username for this URL, if available.
+    // * This account's username.
+    // * "git"
+    //
+    // We have to restart the authentication session each time (due to
+    // constraints in libssh2 I guess? maybe this is inherent to ssh?), so we
+    // call our callback, `f`, in a loop here.
+    if ssh_username_requested {
+        debug_assert!(res.is_err());
+        let mut attempts = Vec::new();
+        attempts.push("git".to_string());
+        if let Ok(s) = ::std::env::var("USER").or_else(|_| ::std::env::var("USERNAME")) {
+            attempts.push(s);
+        }
+        if let Some(ref s) = cred_helper.username {
+            attempts.push(s.clone());
+        }
+
+        while let Some(s) = attempts.pop() {
+            // We should get `USERNAME` first, where we just return our attempt,
+            // and then after that we should get `SSH_KEY`. If the first attempt
+            // fails we'll get called again, but we don't have another option so
+            // we bail out.
+            let mut attempts = 0;
+            res = f(&mut |_url, username, allowed| {
+                if allowed.contains(git2::USERNAME) {
+                    return git2::Cred::username(&s);
+                }
+                if allowed.contains(git2::SSH_KEY) {
+                    debug_assert_eq!(Some(&s[..]), username);
+                    attempts += 1;
+                    if attempts == 1 {
+                        ssh_agent_attempts.push(s.to_string());
+                        return git2::Cred::ssh_key_from_agent(&s)
+                    }
+                }
+                Err(git2::Error::from_str("no authentication available"))
+            });
+
+            // If we made two attempts then that means:
+            //
+            // 1. A username was requested, we returned `s`.
+            // 2. An ssh key was requested, we returned to look up `s` in the
+            //    ssh agent.
+            // 3. For whatever reason that lookup failed, so we were asked again
+            //    for another mode of authentication.
+            //
+            // Essentially, if `attempts == 2` then in theory the only error was
+            // that this username failed to authenticate (e.g. no other network
+            // errors happened). Otherwise something else is funny so we bail
+            // out.
+            if attempts != 2 {
+                break
+            }
+        }
+    }
+
+    if res.is_ok() || !any_attempts {
+        return res.map_err(From::from)
+    }
+
+    // In the case of an authentication failure (where we tried something) then
+    // we try to give a more helpful error message about precisely what we
+    // tried.
+    res
+}
