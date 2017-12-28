4fe5332be75743f55694ea962c8e95894931f504
Gabriella Chronis <gchronis@polysync.io>
2017-12-28 11:26:59 -0800

    implement serialize for push entry

--- src/common/push_entry.rs
+++ src/common/push_entry.rs
@@ -8,9 +8,11 @@ use git2::{Oid, Reference, Repository};
 
 use common::Nonce;
 
-use serde_json::Error;
+use serde_json::{self, Error};
+use serde::ser::{Serialize, Serializer, SerializeStruct};
 
-//#[derive(Serialize, Deserialize)]
+
+//#[derive(Deserialize)]
 pub struct PushEntry {
     related_commits: Vec<Oid>,
     branch: String,
@@ -18,6 +20,29 @@ pub struct PushEntry {
     prev_hash: String,
     nonce_bag: HashSet<Nonce>,
     signature: String,
+}
+
+impl Serialize for PushEntry {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer {
+        let mut state = serializer.serialize_struct("PushEntry", 6)?;
+
+        let mut related_commits = self.related_commits.iter()
+                                .map(|oid| { oid.to_string() })
+                                .collect::<Vec<_>>();
+                                state.serialize_field("branch", &self.branch)?;
+        let head = match self.head {
+            Some(oid) => oid.to_string(),
+            None => String::from(""),
+        };
+
+        state.serialize_field("related_commits", &related_commits)?;
+        state.serialize_field("head", &head)?;
+        state.serialize_field("prev_hash", &self.prev_hash)?;
+        state.serialize_field("nonce_bag", &self.nonce_bag)?;
+        state.serialize_field("signature", &self.signature)?;
+        state.end()
+    }
 }
 
 impl PushEntry {
@@ -60,6 +85,10 @@ impl PushEntry {
         })
     }
 
+    pub fn to_str(&self) -> Result<String, serde_json::Error> {
+        serde_json::to_string(self)
+    }
+
     pub fn from_ref(reference: &Reference) -> Option<PushEntry> {
         match reference.target() {
             Some(oid) => PushEntry::from_oid(oid),
@@ -87,14 +116,31 @@ impl PushEntry {
 impl fmt::Display for PushEntry {
     //TODO Implement
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Unimplemented")
+        write!(f, "{}", "not implemented")
     }
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
+    use super::*;
+
     fn it_works(){
 
+    }
+
+    #[test]
+    fn to_string() {
+        let oid = Oid::from_str("decbf2be529ab6557d5429922251e5ee36519817").unwrap();
+        let push_entry = PushEntry {
+            related_commits: vec![oid.to_owned(), oid.to_owned()],
+            branch: String::from("branch_name"),
+            head: None,
+            prev_hash: String::from("fwjjk42ofw093j"),
+            nonce_bag: HashSet::new(),
+            signature: String::from("gpg signature"),
+
+        };
+        let serialized = "{\"branch\":\"branch_name\",\"related_commits\":[\"decbf2be529ab6557d5429922251e5ee36519817\",\"decbf2be529ab6557d5429922251e5ee36519817\"],\"head\":\"\",\"prev_hash\":\"fwjjk42ofw093j\",\"nonce_bag\":[],\"signature\":\"gpg signature\"}";
+        assert_eq!(&push_entry.to_str().unwrap(), &serialized)
     }
 }
