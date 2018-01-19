a19b72dcdc4f75724825a6322126dc1726f53f25
Gabriella Chronis <gchronis@polysync.io>
2018-01-19 12:07:50 -0800

    Add git fixtures to version control
    Prior to this commit, the git repository fixture was stored in a directory
    called `\.git`, and thus was not detected by version control. This commit
    changes the name, allowing it to be added vc.

--- src/utils/test_helper.rs
+++ src/utils/test_helper.rs
@@ -11,17 +11,23 @@ use rand::{Rng, thread_rng};
 
 pub fn setup() -> Result<Repository>  {
     let mut fixture_dir = env::current_dir().unwrap();
-    &fixture_dir.push("fixtures/.git");
+    &fixture_dir.push("fixtures/fixture.git");
+
     let suffix: String = thread_rng().gen_ascii_chars().take(12).collect();
-    let dir_name = format!("/tmp/rsl_test{}", suffix);
-    let path_to = Path::new(&dir_name);
-    create_all(&path_to, true)?;
+    let repo_name = format!("/tmp/rsl_test{}", suffix);
+    let path_to_repo = Path::new(&repo_name);
+    let git_dir = Path::new(".git");
+    let full_path_to_git_dir = path_to_repo.join(git_dir);
     let mut options = CopyOptions::new();
     options.overwrite = true;
+    
+    create_all(&full_path_to_git_dir, true)?;
+    copy(fixture_dir, path_to_repo, &options)?;
 
-    copy(fixture_dir, path_to, &options)?;
+    let orig_name = path_to_repo.join("fixture.git");
+    fs::rename(orig_name.as_path(), full_path_to_git_dir)?;
 
-    match Repository::open(&path_to) {
+    match Repository::open(&path_to_repo) {
         Ok(repo) => Ok(repo),
         Err(e) => panic!("setup failed: {:?}", e),
     }
