1bd5fba0ea64474b94f45825b0dc18b5d90c8998
Gabriella Chronis <gchronis@polysync.io>
2018-01-30 11:24:24 -0800

    Create local and remote test directories
    Prior to this commit, the test setup only created one test repo. This commit
    improves it by creating a mock remote repo and cloning a mock local repo
    from it, in order to test functions that involve communication between two
    repos. By cloning the local from the remote, the remote is automatically set
    as remote `origin`. In addition, the NONCE file is copied directly into the
    local developer dir from a separate test ficture, since it shouldn't exist in
    the remote repository and doesn't get copied by the clone procedure.

--- src/utils/test_helper.rs
+++ src/utils/test_helper.rs
@@ -12,26 +12,41 @@ use rand::{Rng, thread_rng};
 pub fn setup() -> Repository  {
     let mut fixture_dir = env::current_dir().unwrap();
     &fixture_dir.push("fixtures/fixture.git");
-
     let suffix: String = thread_rng().gen_ascii_chars().take(12).collect();
-    let repo_name = format!("/tmp/rsl_test{}", suffix);
-    let path_to_repo = Path::new(&repo_name);
+
+
+    // create remote repo dir and copy .git from fixture
+    let remote_repo_name = format!("/tmp/rsl_test{}_remote", suffix);
+    let path_to_remote_repo = Path::new(&remote_repo_name);
     let git_dir = Path::new(".git");
-    let full_path_to_git_dir = path_to_repo.join(git_dir);
+    let full_path_to_git_dir = path_to_remote_repo.join(git_dir);
     let mut options = CopyOptions::new();
     options.overwrite = true;
 
     create_all(&full_path_to_git_dir, true).unwrap();
-    copy(fixture_dir, path_to_repo, &options).unwrap();
+    copy(fixture_dir, path_to_remote_repo, &options).unwrap();
 
-    let orig_name = path_to_repo.join("fixture.git");
+    let orig_name = path_to_remote_repo.join("fixture.git");
     fs::rename(orig_name.as_path(), full_path_to_git_dir).unwrap();
 
-    let repo = match Repository::open(&path_to_repo) {
+
+    // create local developer directory and clone repo from remote
+    let local_repo_name = format!("/tmp/rsl_test{}_local", suffix);
+    let path_to_local_repo = Path::new(&local_repo_name);
+    create_all(&path_to_local_repo, true).unwrap();
+    let remote_url = format!("file://{}", &path_to_remote_repo.to_str().unwrap());
+    Repository::clone(&remote_url, &path_to_local_repo).unwrap();
+
+    // add local Nonce
+    fixture_dir = env::current_dir().unwrap();
+    &fixture_dir.push("fixtures/fixture.NONCE");
+    fs::copy(fixture_dir, &path_to_local_repo.join(".git").join("NONCE")).unwrap();
+
+    let local_repo = match Repository::open(&path_to_local_repo) {
         Ok(repo) => repo,
         Err(e) => panic!("setup failed: {:?}", e),
     };
-    repo
+    local_repo
 }
 
 pub fn teardown(repo: &Repository) -> Result<()> {
