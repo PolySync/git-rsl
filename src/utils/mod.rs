486201ecaf1e3f52f0f80ec3c1dc4effdcb2fcef
Gabriella Chronis <gchronis@polysync.io>
2018-02-14 16:24:55 -0800

    Add hex conversion serializer functions
    Using the solution found here:
    https://github.com/serde-rs/serde/issues/661
    Serializer and Deserializer methods for converting to and from hex-encoded
    git SHAs and their lib2git representation as byte arrays.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,29 @@
+use hex::{self, FromHex, ToHex};
+use serde::{Serializer, Deserialize, Deserializer};
+
+
 pub mod test_helper;
 pub mod git;
 pub mod gpg;
 
 fn run_successfully() {
     // TODO implement
+}
+
+
+/// Serializes `buffer` to a lowercase hex string.
+pub fn buffer_to_hex<T, S>(buffer: &T, serializer: S) -> Result<S::Ok, S::Error>
+  where T: AsRef<[u8]>,
+        S: Serializer
+{
+  serializer.serialize_str(&hex::encode(buffer))
+}
+
+/// Deserializes a lowercase hex string to a `Vec<u8>`.
+pub fn hex_to_buffer<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+  where D: Deserializer<'de>
+{
+  use serde::de::Error;
+  String::deserialize(deserializer)
+    .and_then(|string| Vec::from_hex(&string).map_err(|err| Error::custom(err.to_string())))
 }
