014e2c16426799afdea2fc60bc00a504b81009ab
Katie Cleary <kcleary@polysync.io>
2018-06-12 15:56:53 -0700

    Add integration test for ref-check-before-push
    Prior to this commit, we did not have a test to ensure that metadata attacks are now detected through push operations as well as fetch. This commit adds a test to ensure that push operations now detect RSL inconsistencies.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,12 +1,19 @@
 #[macro_use]
 extern crate lazy_static;
 
+extern crate tempdir;
+extern crate names;
 extern crate git_rsl;
 extern crate git2;
+
+mod utils; 
+
 use std::process::Command;
 use std::sync::Mutex;
 use git_rsl::{BranchName, RemoteName};
 use git_rsl::utils::test_helper::*;
+use git_rsl::errors::{Error, ErrorKind};
+use utils::attack;
 
 lazy_static! {
     static ref SEQUENTIAL_TEST_MUTEX: Mutex<()> = Mutex::new(());
@@ -86,6 +93,29 @@ sequential_test! {
 
             let res2 = git_rsl::secure_push_with_cleanup(&mut context.local, &RemoteName::new("origin"), &BranchName::new("master")).expect("Second push failed");
             assert_eq!(res2, ());
+        }
+        teardown_fresh(context)
+    }
+}
+
+sequential_test! {
+    fn attack_detected_on_push() {
+        let mut context = setup_fresh();
+        {
+            assert_eq!((), git_rsl::rsl_init_with_cleanup(&mut context.local, &RemoteName::new("origin"))
+                .expect("Could not rsl-init"));
+            let res = git_rsl::secure_push_with_cleanup(&mut context.local, &RemoteName::new("origin"), &BranchName::new("master")).expect("First push failed");
+            assert_eq!(res, ());
+            do_work_on_branch(&context.local, "refs/heads/master");
+
+            let res2 = git_rsl::secure_push_with_cleanup(&mut context.local, &RemoteName::new("origin"), &BranchName::new("master")).expect("Second push failed");
+            assert_eq!(res2, ());
+
+            attack::rollback(&context.remote, "master");
+
+            do_work_on_branch(&context.local, "refs/heads/master");
+            let res3 = git_rsl::secure_push_with_cleanup(&mut context.local, &RemoteName::new("origin"), &BranchName::new("master")).expect_err("Checking for invalid RSL detection");
+            assert_eq!(res3.description(), "invalid remote RSL");
         }
         teardown_fresh(context)
     }
