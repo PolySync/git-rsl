ab34ee52ff378dace533b283ae18a725ffd4b5fb
Katie Cleary <kcleary@polysync.io>
2018-06-14 14:51:14 -0700

    Re-add git tests
    Prior to this commit, the git tests had been removed for reasons. This commit re-adds them, because it's always good to have more tests to make sure things are working.

--- tests/utils/git.rs
+++ tests/utils/git.rs
@@ -2,8 +2,8 @@ use std::path::{Path, PathBuf};
 use std::env;
 use std::process::{Command, Stdio};
 use git2::{Repository, BranchType};
-use tempdir::TempDir;
 use names::Generator;
+use git_rsl::utils::test_helper::*;
 
 fn set_gnupg_home() {
     let fixtures_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("fixtures");
@@ -60,3 +60,91 @@ pub fn branch(repo: &Repository, name: &str) -> bool {
 pub fn checkout(repo: &Repository, branch_name: &str) -> bool {
     Command::new("git").stdout(Stdio::null()).stderr(Stdio::null()).args(&["checkout", branch_name]).current_dir(repo.workdir().expect("failed to get workdir of repo")).status().expect("failed to perform git checkout").success()
 }
+
+#[test]
+fn git_branch() {
+    let mut context = setup_fresh();
+    {
+        let random_branch_name = Generator::default().next().expect("failed to get a name");
+
+        assert!(branch(&context.local, &random_branch_name));
+
+        assert!(context.local.find_branch(&random_branch_name, BranchType::Local).is_ok());
+    }
+    teardown_fresh(context);
+}
+
+#[test]
+fn git_checkout() {
+    let mut context = setup_fresh(); 
+    {
+        let random_master_commit_msg = Generator::default().next().expect("failed to get a name");
+        commit(&context.local, &random_master_commit_msg);
+
+        let random_branch_name = Generator::default().next().expect("failed to get a name");
+        branch(&context.local, &random_branch_name);
+        assert!(checkout(&context.local, &random_branch_name));
+        let random_branch_commit_msg = Generator::default().next().expect("failed to get a name");
+        commit(&context.local, &random_branch_commit_msg);
+        
+        let local = &context.local;
+
+        let mut commit = local.head().expect("failed to get head").peel_to_commit().expect("failed to get latest commit...");
+        assert!(commit.summary() == Some(&random_branch_commit_msg));
+
+        assert!(checkout(&context.local, "master"));
+        commit = local.head().expect("failed to get head").peel_to_commit().expect("failed to get latest commit...");
+        assert!(commit.summary() == Some(&random_master_commit_msg));
+    }
+    teardown_fresh(context);
+}
+
+#[test]
+fn git_commit() {
+    let mut context = setup_fresh();
+    {
+        let random_commit_msg = Generator::default().next().expect("failed to get a name");
+
+        assert!(commit(&context.local, &random_commit_msg));
+
+        let commit = context.local.head().expect("failed to get head").peel_to_commit().expect("failed to get latest commit...");
+
+        assert!(commit.summary() == Some(&random_commit_msg));
+    }
+    teardown_fresh(context);
+}
+
+#[test]
+fn git_push() {
+    let mut context = setup_fresh();
+    {
+        commit(&context.local, "C2");
+
+        let oid = context.local.head().expect("failed to get head").target().expect("failed to get C2 oid...");
+        assert!(&context.remote.find_commit(oid).is_err());
+
+        push(&context.local, "master");
+        assert!(&context.remote.find_commit(oid).is_ok());
+    }
+    teardown_fresh(context);
+}
+
+// #[test]
+// fn git_pull() {
+//     let temp_dir = TempDir::new("test").expect("failed to create temporary directory");
+//     let (main, mut locals) = super::create_system_state(&temp_dir.path(), 2);
+//     let local_1 = &mut locals.pop().expect("failed to pop repo");
+//     let local_2 = &mut locals.pop().expect("failed to pop repo");
+
+//     commit(local_1, "C2");
+//     let oid = local_1.head().expect("failed to get head").target().expect("failed to get C2 oid...");
+//     assert!(main.find_commit(oid).is_err());
+
+//     push(local_1, "master");
+
+//     assert!(local_2.find_commit(oid).is_err());
+
+//     pull(local_2, "master");
+
+//     assert!(local_2.find_commit(oid).is_ok());
+// }
